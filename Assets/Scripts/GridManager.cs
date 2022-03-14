e06e6eee4a938634b52132b4bbbf19c1a6f82af8
Osama <95348282+OsamaRashid585@users.noreply.github.com>
2022-03-14 07:23:02 -0700

    Update GridManager.cs

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GridManager : MonoBehaviour
 {
     private int _row = 8;
     private int _colum = 8;
-   [SerializeField]  private GameObject _bluePrefab;
+    private int i = 0;
+    [SerializeField]  private GameObject _bluePrefab;
    [SerializeField] private GameObject _pinkPrefab;
     public GameObject[,] _grid;
 
+    //public List<GameObject> BirdTxt = new List<GameObject>();
+     public List<string> BirdsName = new List<string>();
+     public Dictionary<string,GameObject> _text = new Dictionary<string,GameObject>();
+    public GameObject _birdtText;
     private void Start()
     {
         AssignBlock();
         StartCoroutine(GenerateGrid());
-
     }
 
     private void AssignBlock()
@@ -43,13 +47,22 @@ public class GridManager : MonoBehaviour
         {
             for (int col = 0; col < _colum; col++)
             {
+
                 if ((row + col) % 2 == 0)
                 {
                     Instantiate(_grid[row, col], new Vector3(row, 0, col), Quaternion.identity);
+
+                    _text.Add(BirdsName[i], Instantiate(_birdtText, transform.position = new Vector3(col, 0, row), Quaternion.Euler(90, 0, 0)));
+                    _text[BirdsName[i]].GetComponent<TextMesh>().text = BirdsName[i];
+                    i++;
                 }
                 else
                 {
                     Instantiate(_grid[row, col], new Vector3(row, 0, col), Quaternion.identity);
+
+                    _text.Add(BirdsName[i], Instantiate(_birdtText, transform.position = new Vector3(col, 0, row), Quaternion.Euler(90, 0, 0)));
+                    _text[BirdsName[i]].GetComponent<TextMesh>().text = BirdsName[i];
+                    i++;
                 }
                 yield return new WaitForSeconds(0.1f);
             }
